da3d6a732a2668b968573aa463e80a9a72cc7f53
mkaubr007 <mkaubr007@gmail.com>
2021-08-29 21:48:27 +0530

    fibonacci series

--- src/main/java/com/biz/logical/program/FibonacciSeries.java
+++ src/main/java/com/biz/logical/program/FibonacciSeries.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class FibonacciSeries {
     public static void main(String[] args) {
         int n1=0,n2=1;
-        Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);//taking input from user
         System.out.println("Enter no of count");
         int count=sc.nextInt();
-        System.out.print(n1+" "+n2);
-        for (int i=2;i<count;i++){
+        System.out.print(n1+" "+n2);//print default value
+        for (int i=2;i<count;i++){//run loop up to count
             int n3=n1+n2;
-            System.out.print(" "+n3);
+            System.out.print(" "+n3);//print the result
             n1=n2;
             n2=n3;
         }
