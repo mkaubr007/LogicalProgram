517b58a3ceb537d04ec46cbbd69abccdc2a77989
mkaubr007 <mkaubr007@gmail.com>
2021-09-01 11:16:01 +0530

    CouponCode added

--- src/main/java/com/biz/logical/program/CouponCode.java
+++ src/main/java/com/biz/logical/program/CouponCode.java
@@ -1,24 +1,57 @@
 package com.biz.logical.program;
+import java.util.Random;
+import java.util.Scanner;
 
 public class CouponCode {
-    public static void main(String[] args) {
 
-        char[] chars = "abcdefghijklmnop".toCharArray();
+    int[] couponArray;	//	array to store numbers which has already occurred
+    int  pos = 0;	//	position to which the new generated number is to add
+    int randomCount = 0;	//	keeps number of trials required
+    public static void main(String[] args){
 
-        int max = 100000000;
+        CouponCode couponNumbers = new CouponCode();	//	class object
 
-        double d = Math.random();
-        int random = (int) (d * max);
-        String sb = "";
-        System.out.println("random double " + d + " random at start " + random);
+        couponNumbers.start();
 
-        while (random > 0) {
-            sb = sb + (chars[random % chars.length]);
-            random /= chars.length;
-            System.out.println(random + "  " + random % chars.length + " sb is " + sb);
+    }
+
+    // Asks user input
+    void start() {
+        Scanner scanner = new Scanner(System.in);	//	to get user input
+        System.out.print("Enter Number of Coupons: ");
+        int couponCount = scanner.nextInt();
+        scanner.close();
+
+        //initialising array
+        couponArray = new int[couponCount];
+
+        random(couponCount);
+    }
+
+    // generates random number and adds to the array
+    void random(int couponCount){
+        randomCount++;
+        Random random = new Random();
+        int randomInteger = random.nextInt(couponCount) + 1;
+        if(isPresent(randomInteger))
+            random(couponCount);
+        else{
+            couponArray[pos] = randomInteger;
+            pos++;
+            if(pos == couponCount){
+                System.out.println("Number of trials required: " + randomCount);
+                return;
+            }
+            random(couponCount);
         }
+    }
 
-        String couponCode = sb.toString();
-        System.out.println("Coupon Code: " + couponCode);
+    // checks if the generated number has occurred before
+    boolean isPresent(int n){
+        for(int i =0; i < couponArray.length; i++){
+            if(couponArray[i] == n)
+                return true;
+        }
+        return false;
     }
 }
